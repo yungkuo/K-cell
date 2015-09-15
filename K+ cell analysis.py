d9cbf2b57da336b8d9314e0d526e5a4c7fce0c2e
Yung <kuo.yung@gmail.com>
2015-09-15 14:43:27 -0700

    update

--- K+ cell analysis.py
+++ K+ cell analysis.py
@@ -12,16 +12,20 @@ import libtiff
 from sub import polygon
 
 savefig = 1
-abc = 'c'
+abc = 'b'
 squareROI = 1 #1 = squareROI with area: (scan*2)*(scan*2); 0 = polygon ROI
-scan = 30
-filePath = '/Users/yungkuo/Documents/Data/K+ cell/091215 TMRM HEK K potential/TMRM in DMEM_250nM_20min__4/Pos0/'
+scan = 20
+DiOboundary = 0
+if DiOboundary == 1:
+    squareROI = 0
+
+filePath = '/Users/yungkuo/Documents/Data/K+ cell/091415 TMRM DiO HEK K potential/DiO HBS_TMRM DMEM w FBS_250nM_30min_dish1__1/Pos0/'
 param = open(filePath+'metadata.txt', 'r')
 for count, line in enumerate(param.readlines()):
     if count == 4:
         print line
         interval = float(line.split(':')[1].split(',')[0])
-    if count == 39:
+    if count == 41:
         print line
         frame = float(line.split(':')[1].split(',')[0])
 
@@ -29,6 +33,17 @@ def listdir_nohidden(path):
     for f in os.listdir(path):
         if f.endswith('_Green_000.tif'):
             yield f
+
+if DiOboundary == 1:
+    mov = libtiff.TiffFile(filePath+'img_000000000_Blue_000.tif')
+    mov = np.array(mov.get_tiff_array()[:,:,:], dtype='d')
+    mov = np.squeeze(mov)
+    fig, ax = plt.subplots()
+    cax = ax.imshow(mov, interpolation='none', cmap='YlGn')
+    cbar = fig.colorbar(cax)
+    print("choose DiO boundary")
+    pts = plt.ginput(0,0)
+    pts = np.array(pts)
 
 I = []
 t = []
@@ -67,7 +82,31 @@ for count, file in enumerate(listdir_nohidden(filePath)):
         cax = axarr[count/a, count%a].imshow(
               mov[pts[:,1]-scan:pts[:,1]+scan,pts[:,0]-scan:pts[:,0]+scan],
               interpolation='none', cmap='afmhot', vmin=0, vmax=10000)
-    if squareROI == 0:
+    if DiOboundary == 0:
+        if squareROI == 0:
+            if count == 0:
+                #frame=len(mov[:,0,0])
+                nrow=len(mov[:,0])
+                ncol=len(mov[0,:])
+                fig1, ax = plt.subplots()
+                cax = ax.imshow(mov, interpolation='none', cmap='afmhot')
+                cbar = fig1.colorbar(cax)
+                print("Choose polygon ROI by clicking multiple points")
+                print("If finished, press enter")
+                pts = plt.ginput(0,0)
+                pts = np.array(pts)
+                ax.plot(pts[:,0],pts[:,1], '-o')
+                ax.set_xlim([0,ncol])
+                ax.set_ylim([nrow,0])
+            I1, img = polygon.mean_polygon(mov, pts)
+            I = np.append(I, I1)
+            cax = axarr[count/a, count%a].imshow(
+                  img[np.min(pts[:,1]):np.max(pts[:,1]),np.min(pts[:,0]):np.max(pts[:,0])],
+                  interpolation='none', cmap='afmhot', vmin=0, vmax=10000)
+        axarr[count/a, count%a].set_xticks([])
+        axarr[count/a, count%a].set_yticks([])
+        fig2.canvas.draw()
+    if DiOboundary == 1:
         if count == 0:
             #frame=len(mov[:,0,0])
             nrow=len(mov[:,0])
@@ -75,27 +114,26 @@ for count, file in enumerate(listdir_nohidden(filePath)):
             fig1, ax = plt.subplots()
             cax = ax.imshow(mov, interpolation='none', cmap='afmhot')
             cbar = fig1.colorbar(cax)
-            print("Choose polygon ROI by clicking multiple points")
-            print("If finished, press enter")
-            pts = plt.ginput(0,0)
-            pts = np.array(pts)
             ax.plot(pts[:,0],pts[:,1], '-o')
             ax.set_xlim([0,ncol])
             ax.set_ylim([nrow,0])
         I1, img = polygon.mean_polygon(mov, pts)
         I = np.append(I, I1)
         cax = axarr[count/a, count%a].imshow(
-              img[np.min(pts[:,1]):np.max(pts[:,1]),np.min(pts[:,0]):np.max(pts[:,0])],
-              interpolation='none', cmap='afmhot', vmin=0, vmax=10000)
-    axarr[count/a, count%a].set_xticks([])
-    axarr[count/a, count%a].set_yticks([])
-    fig2.canvas.draw()
+            img[np.min(pts[:,1]):np.max(pts[:,1]),np.min(pts[:,0]):np.max(pts[:,0])],
+            interpolation='none', cmap='afmhot', vmin=0, vmax=10000)
+        axarr[count/a, count%a].set_xticks([])
+        axarr[count/a, count%a].set_yticks([])
+        fig2.canvas.draw()
+
+
 t = np.array(t, dtype=float)*interval
 cbar = fig2.colorbar(cax)
 fig3, ax = plt.subplots()
 ax.plot(t, I,'-o')
 if squareROI == 1:
     ax.plot(t, I-bg, '-rs')
+    ax.plot(t, bg, '-g^')
 ax.set_xlabel('Time (ms)')
 ax.set_ylabel('Fluorescence intensity')
 if savefig == 1:
